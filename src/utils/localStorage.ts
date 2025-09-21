cfcb33e847202ce802a27faa0e50c4b8fc732da3
laidalavenia <laida.vanida@gmail.com>
2025-09-21 21:23:54 +0700

    feat: add data local storage

--- src/utils/localStorage.ts
+++ src/utils/localStorage.ts
@@ -9,7 +9,7 @@ const initialMovies: Movie[] = [
     title: "Harry Potter and the Deathly Hallows: Part 2",
     director: "David Yates",
     summary:
-      "Harry, Ron, and Hermione prepare for the final battle against Lord Voldemort as the fate of the wizarding world hangs in the balance.",
+      "Harry, Ron, and Hermione prepare for the final battle against Lord Voldemort",
     genres: ["Action", "Drama"],
     imageUrl:
       "https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p7953526_p_v8_aa.jpg",
@@ -21,20 +21,19 @@ const initialMovies: Movie[] = [
     title: "Moana: The Princess of the Ocean",
     director: "Ron Clements & John Musker",
     summary:
-      "An adventurous Polynesian teenager sets sail on a daring journey to save her island, guided by the demigod Maui and her own courage.",
+      "An adventurous Polynesian teenager sets sail on a daring journey to save her island",
     genres: ["Animation", "Drama"],
     imageUrl:
       "https://lumiere-a.akamaihd.net/v1/images/p_moana_20530_214883e3.jpeg",
     createdAt: "2025-08-10T12:00:00Z",
     updatedAt: "2025-08-10T12:00:00Z",
   },
-
   {
     id: "3",
     title: "The City of Gold",
     director: "Various",
     summary:
-      "English Movies: Watch English Movies 2021, Latest Hollywood Movies Online | English Films",
+      "English Movies: Watch English Movies 2021, Latest Hollywood Movies Online ",
     genres: ["Action"],
     imageUrl:
       "https://m.media-amazon.com/images/M/MV5BZTEyMjBkNzktNzEwNC00ZTY0LTgxYjYtYWEyOThjZGQ2ZDE5XkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg",
@@ -46,7 +45,7 @@ const initialMovies: Movie[] = [
     title: "Jumanji",
     director: "Jake Kasdan",
     summary:
-      "Welcome to The Jungle (English) Movie: Review | Release Date | Songs | Music | Images",
+      "Welcome to The Jungle (English) Movie: Review | Release Date | Songs | Music",
     genres: ["Action"],
     imageUrl:
       "https://www.sonypictures.ie/sites/ireland/files/2021-01/Jumanji-keyArt_1.jpg",
